4692ff6223cfcccd9e2c3ab97c8d6a50c1566ca8
Thiago Hirata <hirata@devcase.com.br>
2020-07-30 20:15:53 -0300

    hardcoded "- vazio -"

--- commons/ignore-ast-path.js
+++ commons/ignore-ast-path.js
@@ -22,7 +22,7 @@ function _default(path) {
 
   //todo - pegar das opções estes parâmetros
   var ignoredRegex = [/[a-z][A-Z]/, /[a-zA-Z]\.[a-zA-Z]/, /^http/, /^\//, /\_/, /^\./, /^\#/, /px$/, /^[A-Z][A-Z][A-Z]$/, /^(accept|acceptCharset|accessKey|action|allowFullScreen|alt|async|autoComplete|autoFocus|autoPlay|capture|cellPadding|cellSpacing|challenge|charSet|checked|cite|classID|className|colSpan|cols|content|contentEditable|contextMenu|controls|controlsList|coords|crossOrigin|data|dateTime|default|defer|dir|disabled|download|draggable|encType|form|formAction|formEncType|formMethod|formNoValidate|formTarget|frameBorder|headers|height|hidden|high|href|hrefLang|htmlFor|httpEquiv|icon|id|inputMode|integrity|is|keyParams|keyType|kind|label|lang|list|loop|low|manifest|marginHeight|marginWidth|max|maxLength|media|mediaGroup|method|min|minLength|multiple|muted|name|noValidate|nonce|open|optimum|pattern|placeholder|poster|preload|profile|radioGroup|readOnly|rel|required|reversed|role|rowSpan|rows|sandbox|scope|scoped|scrolling|seamless|selected|shape|size|sizes|span|spellCheck|src|srcDoc|srcLang|srcSet|start|step|style|summary|tabIndex|target|title|type|useMap|value|width|wmode|wrap)$/];
-  var requiredRegex = [/[a-zA-Z]/];
+  var requiredRegex = [/[0-9A-Za-zÀ-ÿ]/];
   var validJsxAttributes = ["label", "aria-label", "title", "placeholder", "hint", "labelPlural", "message"];
   var validObjectPropertyKeys = ["text", "label", "labelPlural", "hint", "message", "placeholder"];
   var validCallExpressionCallees = [/NotificationManager/];
@@ -31,6 +31,8 @@ function _default(path) {
   if (value.indexOf("i18n:") === 0) {
     return false;
   }
+
+  if (value.trim() == '- vazio -') return false;
 
   if (requiredRegex.find(function (regexp) {
     return !value.match(regexp);
